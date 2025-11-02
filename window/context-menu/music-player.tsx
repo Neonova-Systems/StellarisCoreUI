00ec63d34901e65a9d7c37d71caaaf6ee0d8832a
Xaxeric <xaxeric.dev@gmail.com>
2025-11-02 22:48:36 +0700

    feat(MusicPlayer): populate commands list with descriptions and keybindings

--- window/context-menu/music-player.tsx
+++ window/context-menu/music-player.tsx
@@ -1,10 +1,11 @@
 import { spawnContextMenu } from "../ContextMenu"
 
 const commandsList = [
-    { name: "Play", description: "", command: "", keybind: ""},
-    { name: "Pause", description: "", command: "", keybind: ""},
-    { name: "Next", description: "", command: "", keybind: ""},
-    { name: "Previous", description: "", command: "", keybind: ""},
-    { name: "Stop", description: "", command: "", keybind: ""},
+    { name: "Play", description: "Resume playback of the current track.", command: "/usr/bin/playerctl play", keybind: "p"},
+    { name: "Pause", description: "Pause the currently playing track.", command: "/usr/bin/playerctl pause", keybind: "space"},
+    { name: "Next", description: "Skip to the next track in the playlist.", command: "/usr/bin/playerctl next", keybind: "n"},
+    { name: "Previous", description: "Go back to the previous track.", command: "/usr/bin/playerctl previous", keybind: "b"},
+    { name: "Stop", description: "Stop playback completely.", command: "/usr/bin/playerctl stop", keybind: "s"},
+    { name: "Toggle", description: "Toggle between play and pause states.", command: "/usr/bin/playerctl play-pause", keybind: "t"},
 ]
 spawnContextMenu(commandsList);
