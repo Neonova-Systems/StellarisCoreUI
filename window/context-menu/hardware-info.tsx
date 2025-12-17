09d8d987cfb1f65b6697ae4ece34fbab968b8ef2
Xaxeric <xaxeric.dev@gmail.com>
2025-12-17 16:15:40 +0700

    refactor(hardware-info): remove context menu for hardware monitoring toggle command

--- window/context-menu/hardware-info.tsx
+++
@@ -1,6 +0,0 @@
-import { spawnContextMenu } from "../ContextMenu"
-
-const commandsList = [
-    { name: "Toggle", description: "Toggle hardware monitoring graphs visibility", command: "ags request 'toggleHardwareGraph'", keybind: ""},
-]
-spawnContextMenu(commandsList);
