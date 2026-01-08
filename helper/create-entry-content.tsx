c6617151b48473e2aa168490768783b512e6cccc
Xaxeric <xaxeric.dev@gmail.com>
2026-01-08 17:17:10 +0700

    feat(entry-content): add children prop to CreateEntryContent for flexible rendering

--- helper/create-entry-content.tsx
+++ helper/create-entry-content.tsx
@@ -12,9 +12,10 @@ type EntryContentProps = {
     useMarkup?: boolean | Accessor<NonNullable<boolean | undefined>> | undefined
     orientation?: Gtk.Orientation | Accessor<NonNullable<Gtk.Orientation | undefined>> | undefined
     ellipsize?: Pango.EllipsizeMode | Accessor<NonNullable<Pango.EllipsizeMode | undefined>> | undefined
+    children?: JSX.Element | Array<JSX.Element>
 };
 
-export default function CreateEntryContent({ name, value, css, hexpand = false, allowCopy = false, useMarkup = false, orientation = Gtk.Orientation.VERTICAL, ellipsize }: EntryContentProps) {
+export default function CreateEntryContent({ name, value, css, hexpand = false, allowCopy = false, useMarkup = false, orientation = Gtk.Orientation.VERTICAL, ellipsize, children}: EntryContentProps) {
     const valueStr = typeof value === "string" ? value : value?.get() || "";
     return (
         <box orientation={orientation} spacing={orientation == Gtk.Orientation.VERTICAL ? 1.5 : 3.0} hexpand={hexpand}>
@@ -25,16 +26,20 @@ export default function CreateEntryContent({ name, value, css, hexpand = false, 
                 </>
             )}
             <label label={`${name}:`} css={css} halign={Gtk.Align.START} cssClasses={["alt-start-animation"]}/>
-            <label 
-                useMarkup={useMarkup} 
-                cssClasses={["value", "start-animation", allowCopy ? "copyable" : ""]} 
-                css={css} 
-                label={valueStr} 
-                halign={Gtk.Align.START} 
-                wrap 
-                wrapMode={Gtk.WrapMode.CHAR} 
-                ellipsize={ellipsize}
-                cursor={allowCopy ? Gdk.Cursor.new_from_name("pointer", null) : undefined} />
+            <box orientation={Gtk.Orientation.HORIZONTAL} spacing={4} halign={Gtk.Align.FILL} valign={Gtk.Align.START}>
+                {children}
+                <label 
+                    useMarkup={useMarkup} 
+                    cssClasses={["value", "start-animation", allowCopy ? "copyable" : ""]} 
+                    css={css} 
+                    label={valueStr} 
+                    halign={Gtk.Align.START} 
+                    valign={Gtk.Align.START}
+                    wrap 
+                    wrapMode={Gtk.WrapMode.CHAR} 
+                    ellipsize={ellipsize}
+                    cursor={allowCopy ? Gdk.Cursor.new_from_name("pointer", null) : undefined} hexpand />
+            </box>
         </box>
     )
 }
