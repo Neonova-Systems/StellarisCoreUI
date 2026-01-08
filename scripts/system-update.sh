4ad8cccd5e57b0c9c0621d8c74919f3fad078a4b
Xaxeric <xaxeric.dev@gmail.com>
2026-01-08 18:02:31 +0700

    feat(system-update): enhance notifications with consistent icon usage

--- scripts/system-update.sh
+++ scripts/system-update.sh
@@ -4,7 +4,9 @@
 available_upgrade_count_before=$(pacman -Quq | wc -l)
 
 # Notify user that sync is starting
-notify-send "System Update" "Synchronizing package databases..." --expire-time=5000
+notify-send "System Update" "Synchronizing package databases..." \
+    --icon=system-update-symbolic \
+    --expire-time=5000
 
 # Sync repositories using pkexec for GUI password prompt
 if pkexec pacman -Sy; then # RECOMMENDEDL you can install policykit agent like polkit-gnome or polkit-kde agent for asking password in GUI interface 
@@ -16,12 +18,17 @@ if pkexec pacman -Sy; then # RECOMMENDEDL you can install policykit agent like p
     
     # Send appropriate notification
     if [ "$new_packages" -gt 0 ]; then
-        notify-send "System Update" "Found $new_packages new package(s) available for upgrade.\nTotal upgradable packages: $available_upgrade_count_after"
+        notify-send "System Update" "Found $new_packages new package(s) available for upgrade.\nTotal upgradable packages: $available_upgrade_count_after" \
+            --icon=system-update-symbolic
     elif [ "$available_upgrade_count_after" -gt 0 ]; then
-        notify-send "System Update" "Repository synchronized.\n$available_upgrade_count_after package(s) available for upgrade."
+        notify-send "System Update" "Repository synchronized.\n$available_upgrade_count_after package(s) available for upgrade." \
+            --icon=system-update-symbolic
     else
-        notify-send "System Update" "System is up to date. No packages need upgrading."
+        notify-send "System Update" "System is up to date. No packages need upgrading." \
+            --icon=system-update-symbolic
     fi
 else
-    notify-send "System Update Failed" "Failed to synchronize package databases." --urgency=critical
+    notify-send "System Update Failed" "Failed to synchronize package databases." \
+        --icon=dialog-error \
+        --urgency=critical
 fi
