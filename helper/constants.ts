be0ba6cde4883d9302231f976c1997844e8c69be
Xaxeric <xaxeric.dev@gmail.com>
2026-01-08 17:16:56 +0700

    feat(constants): add ICON_DIR constant for asset icon path

--- helper/constants.ts
+++ helper/constants.ts
@@ -1,5 +1,6 @@
 import GLib from "gi://GLib?version=2.0";
 export const HOME_DIR = GLib.get_home_dir();
+export const ICON_DIR = `${HOME_DIR}/.config/ags/assets/icon`;
 export const CACHE_DIR = `${GLib.get_user_cache_dir()}/ags`;
 export const WALLPAPER_JSON = "wallpaper.json";
 export const SIGNAL_JSON = "signal.json";
