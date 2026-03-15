bdeafe7ae3815820398e1e4f4d461edf66093870
Xaxeric <xaxeric.dev@gmail.com>
2026-03-15 17:34:36 +0700

    feat(workspace-info): enhance workspace button with tooltip and cursor, adjust minimum height

--- modules/workspace.tsx
+++ modules/workspace.tsx
@@ -1,23 +1,16 @@
-import { Gtk } from "ags/gtk4"
-import { For, With, createBinding } from 'ags';
+import { Gdk, Gtk } from "ags/gtk4"
+import { For, With, createBinding, createState } from 'ags';
 import AstalHyprland from "gi://AstalHyprland?version=0.1";
 
 export default function Workspaces() {
     const hyprland = AstalHyprland.get_default();
     const workspaces = createBinding(hyprland, "workspaces")((value) => value.sort((a, b) => a.id - b.id)); // sort workspace by id
     const focusedWorkspace = createBinding(hyprland, "focusedWorkspace");
-
-    // Debug only
-    // workspaces.get().map((value) => {
-    //     print(`id: ${value.id} name: ${value.name}`)
-    //     print("monitor: " + value.monitor)
-    //     print("fullscreen: " + value.hasFullscreen)
-    // })
     return (
-        <box cssClasses={["workspace-container"]} css={`min-height: 3px;`} halign={Gtk.Align.FILL} valign={Gtk.Align.FILL} orientation={Gtk.Orientation.HORIZONTAL} spacing={10}>
+        <box cssClasses={["workspace-container"]} css={`min-height: 3.5px;`} halign={Gtk.Align.FILL} valign={Gtk.Align.FILL} orientation={Gtk.Orientation.HORIZONTAL} spacing={10}>
             <For each={workspaces}>
                 {(workspace: any) => (
-                    <button onClicked={() => workspace.focus()}>
+                    <button onClicked={() => workspace.focus()} tooltipText={`Click to change into workspace ${workspace.id}`} cursor={Gdk.Cursor.new_from_name("crosshair", null)}>
                         <With value={focusedWorkspace}>
                             {(value: any) => <box cssClasses={[value.id == workspace.id ? "focused-item" : "item"]} spacing={5} hexpand />}
                         </With>
