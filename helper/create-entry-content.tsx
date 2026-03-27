ca2efd83ff5e423b25334385ca23413d44ef64b1
Xaxeric <xaxeric.dev@gmail.com>
2026-03-27 23:34:58 +0700

    feat(entry-content): enhance CreateEntryContent with addPercentSuffix and valueLabel for improved display handling

--- helper/create-entry-content.tsx
+++ helper/create-entry-content.tsx
@@ -1,12 +1,12 @@
 import { Gdk, Gtk } from "ags/gtk4"
-import { Accessor } from "ags"
+import { Accessor, With } from "ags"
 import { AudioFile, copyToClipboard, playSound } from "./utility";
 import Pango from "gi://Pango";
 import { Align, HOME_DIR } from "./constants";
 
 type EntryContentProps = {
     name?: string | Accessor<string> | undefined;
-    value?: string | Accessor<string> | undefined;
+    value?: string | number | Accessor<string | number> | undefined;
     css?: string | Accessor<string> | undefined;
     hexpand?: boolean | Accessor<NonNullable<boolean | undefined>> | undefined
     allowCopy?: boolean | Accessor<NonNullable<boolean | undefined>> | undefined
@@ -17,16 +17,31 @@ type EntryContentProps = {
     children?: JSX.Element | Array<JSX.Element>
     important?: boolean | Accessor<NonNullable<boolean | undefined>> | undefined
     watchValue?: boolean | Accessor<NonNullable<boolean | undefined>> | undefined
+    addPercentSuffix?: boolean | Accessor<NonNullable<boolean | undefined>> | undefined
 };
 
-export default function CreateEntryContent({ name, value, css, hexpand = false, allowCopy = false, animation = true, useMarkup = false, orientation = Gtk.Orientation.VERTICAL, ellipsize, important = false, watchValue = false, children}: EntryContentProps) {
-    const valueStr = typeof value === "string" ? value : value?.peek() || "";
+export default function CreateEntryContent({ name, value, css, hexpand = false, allowCopy = false, animation = true, useMarkup = false, orientation = Gtk.Orientation.VERTICAL, ellipsize, important = false, watchValue = false, addPercentSuffix = false, children}: EntryContentProps) {
+    const valueStr = typeof value === "string" ? value : typeof value === "number" ? String(value) : value?.peek() || "";
+    const valueLabel = (labelValue: string | number) => (
+        <label 
+            useMarkup={useMarkup} 
+            cssClasses={["value", animation ? "start-animation" : "", allowCopy ? "copyable" : ""]} 
+            css={css} 
+            label={String(labelValue) + (addPercentSuffix && "%")} 
+            halign={Align.LEFT} 
+            valign={Align.LEFT}
+            wrap 
+            wrapMode={Gtk.WrapMode.CHAR} 
+            ellipsize={ellipsize}
+            cursor={allowCopy ? Gdk.Cursor.new_from_name("pointer", null) : undefined} hexpand />
+    )
+    
     return (
         <box orientation={orientation} spacing={orientation == Gtk.Orientation.VERTICAL ? 1.5 : 3.0} hexpand={hexpand}>
             {allowCopy && (
                 <>
                 <Gtk.EventControllerMotion onEnter={() => playSound(AudioFile.Key)} />
-                <Gtk.GestureClick onPressed={() => { copyToClipboard(valueStr); }} />
+                <Gtk.GestureClick onPressed={() => { copyToClipboard(String(valueStr)); }} />
                 </>
             )}
             <box orientation={Gtk.Orientation.HORIZONTAL} spacing={2} halign={Align.FILL} valign={Align.CENTER}>
@@ -35,17 +50,13 @@ export default function CreateEntryContent({ name, value, css, hexpand = false, 
             </box>
             <box orientation={Gtk.Orientation.HORIZONTAL} spacing={4} halign={Align.FILL} valign={Align.LEFT}>
                 {children}
-                <label 
-                    useMarkup={useMarkup} 
-                    cssClasses={["value", animation ? "start-animation" : "", allowCopy ? "copyable" : ""]} 
-                    css={css} 
-                    label={valueStr} 
-                    halign={Align.LEFT} 
-                    valign={Align.LEFT}
-                    wrap 
-                    wrapMode={Gtk.WrapMode.CHAR} 
-                    ellipsize={ellipsize}
-                    cursor={allowCopy ? Gdk.Cursor.new_from_name("pointer", null) : undefined} hexpand />
+                {watchValue && typeof value !== "string" && typeof value !== "number" && value ? (
+                    <With value={value}>
+                        {(v) => valueLabel(v)}
+                    </With>
+                ) : (
+                    valueLabel(valueStr)
+                )}
             </box>
         </box>
     )
