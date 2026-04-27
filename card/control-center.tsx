aa8e4407ed91e0a24418c9fba5c71fd92b1eaf9f
Xaxeric <xaxeric.dev@gmail.com>
2026-04-27 14:34:36 +0700

    feat: add NetworkControl component for network management functionality

--- card/control-center.tsx
+++ card/control-center.tsx
@@ -5,6 +5,7 @@ import AudioControl from "./control-center/audio-control";
 import ControlKey from "./control-center/control-key";
 import HyprlandControl from "./control-center/hyprland-control";
 import { initToggleState, openContextMenu } from "../helper/behaviour";
+import NetworkControl from "./control-center/network-control";
 
 export default function ControlCenter({ onDragUp, onDragDown }: { onDragUp?: () => void, onDragDown?: () => void }) {
     const [toggleContentState, settoggleContentState] = createState(false);
@@ -22,6 +23,7 @@ export default function ControlCenter({ onDragUp, onDragDown }: { onDragUp?: () 
                             <ControlKey />
                             <AudioControl />
                             <HyprlandControl />
+                            <NetworkControl />
                         </box>
                     </box>
                 )}
