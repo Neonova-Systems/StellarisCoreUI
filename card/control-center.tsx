4b7ed005f276f36e794da322bd2b38beb1e143c4
Xaxeric <xaxeric.dev@gmail.com>
2026-04-27 14:35:01 +0700

    feat: add BluetoothControl component for Bluetooth management functionality

--- card/control-center.tsx
+++ card/control-center.tsx
@@ -6,6 +6,7 @@ import ControlKey from "./control-center/control-key";
 import HyprlandControl from "./control-center/hyprland-control";
 import { initToggleState, openContextMenu } from "../helper/behaviour";
 import NetworkControl from "./control-center/network-control";
+import BluetoothControl from "./control-center/bluetooth-control";
 
 export default function ControlCenter({ onDragUp, onDragDown }: { onDragUp?: () => void, onDragDown?: () => void }) {
     const [toggleContentState, settoggleContentState] = createState(false);
@@ -24,6 +25,7 @@ export default function ControlCenter({ onDragUp, onDragDown }: { onDragUp?: () 
                             <AudioControl />
                             <HyprlandControl />
                             <NetworkControl />
+                            <BluetoothControl />
                         </box>
                     </box>
                 )}
---
+++ card/control-center/bluetooth-control.tsx
@@ -0,0 +1,25 @@
+import { Gtk } from "ags/gtk4"
+import { Align, HOME_DIR, ICON_DIR } from "../../helper"
+import { Corner, drawChamferedBackground } from "../../helper/draw-function"
+import CreateUtilityButton from "../../helper/create-utility-button"
+import { execAsync } from "ags/process"
+
+export default function BluetoothControl() {
+    const openBlueman = () => execAsync(`foot -e blueman-manager`)
+    return (
+        <box marginTop={10}>
+            <overlay>
+                <box css={`min-height: 50px;`}>
+                    <drawingarea halign={Align.FILL} valign={Align.FILL} hexpand $={(self) => self.set_draw_func((area, cr, width, height) => drawChamferedBackground({area, cr, width, height, notchSize: 13, backgroundColor: "#000000", backgroundAlpha: 0.13, borderAlpha: 1.0, borderColor: "#0B1233", borderSize: 1.7, notchPlacements: [{corner: Corner.BottomRight}], }))} />
+                </box>
+                <box cssClasses={["content"]} $type="overlay" orientation={Gtk.Orientation.VERTICAL} spacing={5}>
+                    <box spacing={5} valign={Align.TOP} halign={Align.LEFT}>
+                        <image file={`${HOME_DIR}/.config/ags/assets/ornament/frame-01.svg`} pixelSize={15}/>
+                        <label cssClasses={["title"]} label="BLUETOOTH CONTROL"/>
+                        <CreateUtilityButton imageFile={`${ICON_DIR}/majesticons--open.svg`} tooltipText={"Open Blueman manager\n\nPair, connect, and configure\nBluetooth devices"} pixelSize={8} onClicked={openBlueman}/>
+                    </box>
+                </box>
+            </overlay>
+        </box>
+    )
+}
