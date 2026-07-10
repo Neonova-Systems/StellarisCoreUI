0bbdbc27dab0aa97a14c9bc78ee98288b9a25c26
Xaxeric <xaxeric.dev@gmail.com>
2026-07-10 20:52:56 +0700

    Refining global-state-mapping
    Replaced loose index signature indexing ([key: string]: boolean) with strict mapping structure
    converted DashboardState into variable reference syntax `state`

--- service/global-state-mapping-service.ts
+++ service/global-state-mapping-service.ts
@@ -1,6 +1,5 @@
 import { GLOBAL_BOOLEAN_STATE_JSON, readJson, writeJson } from "../helper";
 
-// State management
 const stateKeys = {
   dataStreamVisible: "DataStream",
   systemInfoVisible: "SystemInfo",
@@ -22,30 +21,26 @@ const stateKeys = {
   controlKeyState: "ControlKey"
 } as const;
 
-export type GlobalState = {
-  [K in keyof typeof stateKeys]: boolean;
-} & {
-  [key: string]: boolean;
-}
+export type GlobalState = Record<keyof typeof stateKeys, boolean>;
 
 // Auto-generate defaults (all true)
-const defaultDashboardState: GlobalState = Object.keys(stateKeys).reduce((acc, key) => {
-  (acc as any)[key] = true;
+const defaultDashboardState = Object.keys(stateKeys).reduce((acc, key) => {
+  acc[key as keyof GlobalState] = true;
   return acc;
 }, {} as GlobalState);
 
-let dashboardState = readJson<GlobalState>(GLOBAL_BOOLEAN_STATE_JSON, defaultDashboardState);
+let state = readJson<GlobalState>(GLOBAL_BOOLEAN_STATE_JSON, defaultDashboardState);
 
 // Auto-generate mappings (inverted: "DataStream" -> "dataStreamVisible")
 export const stateMappings = Object.entries(stateKeys).reduce((acc, [key, value]) => {
   acc[value] = key as keyof GlobalState;
   return acc;
-}, {} as { [key: string]: keyof GlobalState });
+}, {} as Record<string, keyof GlobalState>);
 
-export function handleStateChange(key: keyof GlobalState, res: (response: any) => void, toggle = false) {
+export function handleStateChange(key: keyof GlobalState, res: (response: string) => void, toggle = false): void {
   if (toggle) {
-    dashboardState[key] = !dashboardState[key];
-    writeJson(GLOBAL_BOOLEAN_STATE_JSON, dashboardState);
+    state[key] = !state[key];
+    writeJson(GLOBAL_BOOLEAN_STATE_JSON, state);
   }
-  return res(String(dashboardState[key]));
+  res(String(state[key]));
 }
